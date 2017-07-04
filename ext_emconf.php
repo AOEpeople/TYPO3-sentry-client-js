fa8b982dc19bda0fd0f924a91bce2545dd352654
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2017-07-04 10:04:43 +0200

    [TASK] CD-58682 Adjusted ext_emconf.php + bumped version

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.4.0',
+    'version' => '0.4.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-6.2.99',
+            'typo3' => '6.2.0-7.6.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
