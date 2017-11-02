1e3c92cba47472cf6015140115a4fbaf7ce210ad
Juergen Kussmann <juergen.kussmann@aoe.com>
2017-11-02 09:03:43 +0100

    [CLEANUP] don't configure TCA in ext_tables.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,8 +3,9 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+// Register static TypoScript templates
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'sentry_client_js',
     'Configuration/TypoScript',
     'Client-side error reporting with Sentry'
 );
